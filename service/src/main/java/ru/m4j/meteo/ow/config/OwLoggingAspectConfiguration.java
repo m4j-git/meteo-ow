9298474314752eba31984f1f077ff9a00e073c03
woodapiary <git@woodapiary.com>
2021-10-27 23:34:27 +0300

    remove

--- service/src/main/java/ru/m4j/meteo/ow/config/OwLoggingAspectConfiguration.java
+++ service/src/main/java/ru/m4j/meteo/ow/config/OwLoggingAspectConfiguration.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
-import ru.m4j.meteo.ow.aop.OwLoggingAspect;
+import ru.m4j.meteo.aop.LoggingAspect;
+
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -17,7 +18,7 @@ public class OwLoggingAspectConfiguration {
 
     @Bean
     @Profile("dev")
-    public OwLoggingAspect loggingAspect(Environment env) {
-        return new OwLoggingAspect(env);
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
     }
 }
