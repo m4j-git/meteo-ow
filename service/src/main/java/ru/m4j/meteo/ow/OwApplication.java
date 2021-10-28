661d7744a5091a84fed2ef9745b98864dcb515dd
woodapiary <git@woodapiary.com>
2021-10-28 19:41:21 +0300

    cleanup

--- service/src/main/java/ru/m4j/meteo/ow/OwApplication.java
+++ service/src/main/java/ru/m4j/meteo/ow/OwApplication.java
@@ -3,8 +3,6 @@
  */
 package ru.m4j.meteo.ow;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,7 +13,6 @@ import org.springframework.context.annotation.Profile;
 @SpringBootApplication
 @EntityScan(basePackages = {"ru.m4j.meteo.*.domain"})
 public class OwApplication extends SpringBootServletInitializer {
-    private static final Logger log = LoggerFactory.getLogger(OwApplication.class);
 
     public static void main(final String[] args) {
         SpringApplication.run(OwApplication.class, args);
