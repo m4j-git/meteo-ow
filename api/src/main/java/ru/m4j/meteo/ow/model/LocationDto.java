059d7240be189c9d8bd8e2493e962aeea401f6cc
m4j <git@m4j.ru>
2021-11-07 02:11:10 +0300

    add service for location from xml

--- api/src/main/java/ru/m4j/meteo/ow/model/LocationDto.java
+++ api/src/main/java/ru/m4j/meteo/ow/model/LocationDto.java
@@ -11,13 +11,15 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @EqualsAndHashCode
 @AllArgsConstructor
-public class GeonameDto implements Serializable {
+@NoArgsConstructor
+public class LocationDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
