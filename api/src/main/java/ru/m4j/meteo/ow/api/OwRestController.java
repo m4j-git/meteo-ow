2f80c3a368fa2a6d0fdcb7d35403e786c035d9a7
m4j <git@m4j.ru>
2021-11-13 05:03:14 +0300

    set rest only via json

--- api/src/main/java/ru/m4j/meteo/ow/api/OwRestController.java
+++ api/src/main/java/ru/m4j/meteo/ow/api/OwRestController.java
@@ -5,6 +5,7 @@ package ru.m4j.meteo.ow.api;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,24 +15,24 @@ import io.swagger.annotations.ApiOperation;
 import ru.m4j.meteo.ow.model.OwCurrentDto;
 import ru.m4j.meteo.ow.model.OwMessageDto;
 
-@RequestMapping("/api/v1/ow")
+@RequestMapping("/api/v1")
 public interface OwRestController {
 
     @ApiOperation(notes = "openweather messages", value = "get list of weather messages")
-    @GetMapping("/messages")
+    @GetMapping(value = "/messages", produces = MediaType.APPLICATION_JSON_VALUE)
     List<OwMessageDto> getMessages(@RequestParam Integer geonameId, @RequestParam(required = false) String dateFrom,
             @RequestParam(required = false) String dateTo);
 
     @ApiOperation(notes = "openweather messages", value = "get weather message")
-    @GetMapping("/messages/{id}")
-    OwMessageDto getMessage(@PathVariable(value = "id") String id);
+    @GetMapping(value = "/messages/one/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    OwMessageDto getMessage(@PathVariable(value = "uuid") String uuid);
 
     @ApiOperation(notes = "openweather messages", value = "get last weather message")
-    @GetMapping("/messages/last")
+    @GetMapping(value = "/messages/last", produces = MediaType.APPLICATION_JSON_VALUE)
     OwMessageDto getLastMessage(@RequestParam Integer geonameId);
 
     @ApiOperation(notes = "openweather messages", value = "get fact weather messages")
-    @GetMapping("/messages/facts")
+    @GetMapping(value = "/messages/facts", produces = MediaType.APPLICATION_JSON_VALUE)
     List<OwCurrentDto> getFacts(@RequestParam Integer geonameId, @RequestParam(required = false) String dateFrom,
             @RequestParam(required = false) String dateTo);
 }
