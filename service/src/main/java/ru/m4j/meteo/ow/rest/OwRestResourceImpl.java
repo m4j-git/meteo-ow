b36c573c5b13831d25894a2a5259df76a390c6c9
m4j <git@m4j.ru>
2021-12-08 13:08:44 +0300

    up to openapi

--- service/src/main/java/ru/m4j/meteo/ow/rest/OwRestResourceImpl.java
+++ service/src/main/java/ru/m4j/meteo/ow/rest/OwRestResourceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import ru.m4j.meteo.ow.client.OwRestResource;
 import ru.m4j.meteo.ow.model.OwCurrentDto;
 import ru.m4j.meteo.ow.model.OwMessageDto;
@@ -29,28 +29,28 @@ public class OwRestResourceImpl implements OwRestResource {
     }
 
     @Override
-    @ApiOperation(notes = "openweather messages", value = "get list of weather messages")
+    @Operation(tags = "openweather messages", summary = "get list of weather messages")
     @GetMapping(value = "/messages", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<OwMessageDto> getMessages(@RequestParam Integer geonameId, @RequestParam(required = false) String dateFrom,
         @RequestParam(required = false) String dateTo) {
         return messageService.getMessages(geonameId, dateFrom, dateTo);
     }
 
-    @ApiOperation(notes = "openweather messages", value = "get weather message")
+    @Operation(tags = "openweather messages", summary = "get weather message")
     @GetMapping(value = "/messages/one/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Override
     public OwMessageDto getMessage(@PathVariable("uuid") String uuid) {
         return messageService.getMessage(uuid);
     }
 
-    @ApiOperation(notes = "openweather messages", value = "get last weather message")
+    @Operation(tags = "openweather messages", summary = "get last weather message")
     @GetMapping(value = "/messages/last", produces = MediaType.APPLICATION_JSON_VALUE)
     @Override
     public OwMessageDto getLastMessage(@RequestParam Integer geonameId) {
         return messageService.getLastMessage(geonameId);
     }
 
-    @ApiOperation(notes = "openweather messages", value = "get fact weather messages")
+    @Operation(tags = "openweather messages", summary = "get fact weather messages")
     @GetMapping(value = "/messages/facts", produces = MediaType.APPLICATION_JSON_VALUE)
     @Override
     public List<OwCurrentDto> getFacts(@RequestParam Integer geonameId, @RequestParam(required = false) String dateFrom,
