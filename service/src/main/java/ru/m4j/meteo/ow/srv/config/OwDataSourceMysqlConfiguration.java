2302f4bf8ab9db2eefc4ac98ad5bf92462bc14d2
m4j <git@m4j.ru>
2021-12-24 22:25:58 +0300

    <dependency> 					<groupId>de.codecentric</groupId> 					<artifactId>spring-boot-admin-starter-client</artifactId> 				</dependency> 				<dependency> 					<groupId>org.springframework.boot</groupId> 					<artifactId>spring-boot-starter-actuator</artifactId> 				</dependency> 				<dependency> 					<groupId>io.springfox</groupId> 					<artifactId>springfox-swagger-ui</artifactId> 				</dependency>

---
+++ service/src/main/java/ru/m4j/meteo/ow/srv/config/OwDataSourceMysqlConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2002-2021 meteo@m4j.ru
+ */
+package ru.m4j.meteo.ow.srv.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Profile("prod,stage")
+@Configuration
+public class OwDataSourceMysqlConfiguration {
+
+    private Environment environment;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://mysql-host:3306/meteo-ow?&serverTimezone=Europe/Moscow&useUnicode=true&characterEncoding=utf8&useSSL=false&requireSSL=false&allowPublicKeyRetrieval=true");
+        dataSource.setUsername(environment.getProperty("METEO_USER"));
+        dataSource.setPassword(environment.getProperty("METEO_PASSWD"));
+        dataSource.setMaximumPoolSize(3);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        Properties jpaProperties = new Properties();
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setPackagesToScan("ru.m4j.meteo.**.domain");
+        vendorAdapter.setGenerateDdl(false);
+        vendorAdapter.setShowSql(false);
+
+        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, "org.hibernate.dialect.MySQL55Dialect");
+        jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, "validate");
+        jpaProperties.put(org.hibernate.cfg.Environment.USE_NEW_ID_GENERATOR_MAPPINGS, "false");
+
+        return entityManagerFactoryBean;
+    }
+
+}
--- service/src/main/java/ru/m4j/meteo/ow/srv/config/OwJpaConfiguration.java
+++ service/src/main/java/ru/m4j/meteo/ow/srv/config/OwJpaConfiguration.java
@@ -3,16 +3,12 @@
  */
 package ru.m4j.meteo.ow.srv.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "ru.m4j.meteo.*.repo")
-@EntityScan(basePackages = { "ru.m4j.meteo.*.domain" })
 @EnableJpaAuditing
-@EnableTransactionManagement
 public class OwJpaConfiguration {
 }
